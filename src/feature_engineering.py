0dc98e8ce4b5dcbe2dacd21d16a28cef985630b1
rakshithjainn-cmyk <rakshithjainn@gmail.com>
2025-09-03 00:10:58 +0530

    Update and rename feature engineering to src/feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,8 +1,9 @@
-import pandas as pd
 from sklearn.feature_selection import SelectKBest, f_classif
 
 
 def select_features(X, y, k=10):
+if k > X.shape[1]:
+k = X.shape[1]
 selector = SelectKBest(score_func=f_classif, k=k)
 X_new = selector.fit_transform(X, y)
 return X_new, selector.get_support(indices=True)
